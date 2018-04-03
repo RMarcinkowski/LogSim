a58b186bd3660ef85acee4780fbc4fa2cd12f9b4
RMarcinkowski <32728206+RMarcinkowski@users.noreply.github.com>
2018-04-03 14:49:52 +0200

    Update Logfunc.py

--- Logfunc.py
+++ Logfunc.py
@@ -1,4 +1,4 @@
-__version__ = "1.0"                             # Verwaltungsinfos
+__version__ = "1.1"                             # Verwaltungsinfos
 __author__ = "Ruben Marcinkowski"
 
 class AndGate:                                  # Klassendefinition
@@ -52,7 +52,7 @@ class AndGate:                                  # Klassendefinition
 
 
 
-class OrGate:                                  # Klassendefinition
+class OrGate:                                   # Klassendefinition
     def __init__(self):                         # Attribute definieren
         self.__Input0 = False
         self.__Input1 = False
@@ -101,3 +101,62 @@ class OrGate:                                  # Klassendefinition
     Input0 = property(__getInput0, __setInput0)
     Input1 = property(__getInput1, __setInput1)
     Output = property(__getOutput, None)
+
+
+class XorGate:                                  # Klassendefinition
+    def __init__(self):                         # Attribute definieren
+        self.__Input0 = False
+        self.__Input1 = False
+        self.__Output = False
+        self.__Name = ""
+        print("XorGate wurde erstellt")    # for testing purpose only
+
+    # getter and setter
+    def __getInput0(self):
+        return self.__Input0
+
+    def __setInput0(self, input):
+        self.__Input0 = input
+
+    def __getInput1(self):
+        return self.__Input1
+
+    def __setInput1(self, input):
+        self.__Input1 = input
+
+    def __getOutput(self):
+        return self.__Output
+
+    def __getName(self):
+        return self.__Name
+
+    def __setName(self, name):
+        self.__Name = name
+
+
+    # methods
+    def show(self):
+        print(self.__str__())
+
+    def execute(self):
+        if self.Input0 == self.Input1:
+            self.__Output = False
+        else:
+            self.__Output = True
+
+    def __str__(self):
+        return "Input0(" + str(self.Input0) + ") und Input1(" + str(self.Input1) + ") ergibt: " + str(self.Output)
+    
+
+    # properties
+    Name = property(__getName, __setName)
+    Input0 = property(__getInput0, __setInput0)
+    Input1 = property(__getInput1, __setInput1)
+    Output = property(__getOutput, None)
+
+
+# print display test
+x = []
+for i in range(0,10):
+    a = XorGate()
+    x.append(a)
