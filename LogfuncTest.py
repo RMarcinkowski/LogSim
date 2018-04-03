6192c2e455646aa4c5385606059acef5da4dbdbf
RMarcinkowski <32728206+RMarcinkowski@users.noreply.github.com>
2018-04-03 14:50:45 +0200

    Update LogfuncTest.py

--- LogfuncTest.py
+++ LogfuncTest.py
@@ -61,5 +61,34 @@ class OrGateTest(unittest.TestCase):
         self.assertTrue(a.Output, "Class OrGate: Testcase 4 failed.")
 
 
+class XorGateTest(unittest.TestCase):
+    def testcase_01(self):
+        a = XorGate()
+        a.Input0 = False
+        a.Input1 = False
+        a.execute()
+        self.assertFalse(a.Output, "Class XorGate: Testcase 1 failed.")
+
+    def testcase_02(self):
+        a = XorGate()
+        a.Input0 = True
+        a.Input1 = False
+        a.execute()
+        self.assertTrue(a.Output, "Class XorGate: Testcase 2 failed.")
+
+    def testcase_03(self):
+        a = XorGate()
+        a.Input0 = False
+        a.Input1 = True
+        a.execute()
+        self.assertTrue(a.Output, "Class XorGate: Testcase 3 failed.")
+
+    def testcase_04(self):
+        a = XorGate()
+        a.Input0 = True
+        a.Input1 = True
+        a.execute()
+        self.assertFalse(a.Output, "Class XorGate: Testcase 4 failed.")
+
 if __name__ == "__main__":
     unittest.main()
