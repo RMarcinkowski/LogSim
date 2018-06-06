50247dc907e663b40f0ff14c03fa55d35cca5411
RMarcinkowski <32728206+RMarcinkowski@users.noreply.github.com>
2018-06-06 10:08:55 +0200

    Update LogFunc.py

--- LogFunc.py
+++ LogFunc.py
@@ -1,16 +1,19 @@
 from abc import ABC, abstractmethod
 
-__version__ = "4.0"  # Verwaltungsinfos
+__version__ = "4.1"  # Verwaltungsinfos
 __author__ = "Ruben Marcinkowski"
 
 
 class LogFunc(ABC):
     """The parent class for logic gate classes."""
+
     def __init__(self, numInputs, numOutputs):
         """Create a logic gate.
+
         Keyword argument:
-        numInputs -- variable number of inputs (two inputs by default).
-        numOutputs -- variable number of outputs (one output by default).
+        numInputs -- variable number of inputs.
+        numOutputs -- variable number of outputs.
+
         Inputs and output get boolean value false by default.
         """
         self.__Inputs = [False] * numInputs
@@ -28,7 +31,7 @@ class LogFunc(ABC):
     def __getOutputs(self):
         return self.__Outputs
 
-    def _setOutputs(self, outputs):       # setOutputs is protected
+    def _setOutputs(self, outputs):  # setOutputs is protected
         self.__Outputs = outputs
 
     def __getName(self):
@@ -61,8 +64,10 @@ class LogFunc(ABC):
 
 
 class AndGate(LogFunc):
-    def __init__(self, numInputs = 2):        
-        super().__init__(numInputs,1)
+
+    def __init__(self, numInputs=2):
+        """set the number of inputs (default: 2) and one output."""
+        super().__init__(numInputs, 1)
 
     def execute(self):
         """set the outputs to true when all inputs are true."""
@@ -73,8 +78,10 @@ class AndGate(LogFunc):
 
 
 class OrGate(LogFunc):
-    def __init__(self, numInputs = 2):        
-        super().__init__(numInputs,1)
+
+    def __init__(self, numInputs=2):
+        """set the number of inputs (default: 2) and one output."""
+        super().__init__(numInputs, 1)
 
     def execute(self):
         """set the outputs to false when all inputs are false."""
@@ -85,8 +92,10 @@ class OrGate(LogFunc):
 
 
 class XorGate(LogFunc):
-    def __init__(self, numInputs = 2):        
-        super().__init__(numInputs,1)
+
+    def __init__(self, numInputs=2):
+        """set the number of inputs (default: 2) and one output."""
+        super().__init__(numInputs, 1)
 
     def execute(self):
         """set the outputs to true when exactly one input is true."""
@@ -97,8 +106,10 @@ class XorGate(LogFunc):
 
 
 class NandGate(LogFunc):
-    def __init__(self, numInputs = 2):        
-        super().__init__(numInputs,1)
+
+    def __init__(self, numInputs=2):
+        """set the number of inputs (default: 2) and one output."""
+        super().__init__(numInputs, 1)
 
     def execute(self):
         """set the outputs to false when all inputs are true."""
@@ -107,13 +118,30 @@ class NandGate(LogFunc):
         else:
             self._setOutputs(True)
 
+
 class NotGate(LogFunc):
-    def __init__(self, numInputs = 2):        
+
+    def __init__(self, numInputs=2):
+        """set the number of inputs and outputs (default: 2)."""
         super().__init__(numInputs, numInputs)
-        
+
     def execute(self):
         """set the output at the specific index to the opposite of the input at that position."""
         outputs = [None] * len(self.Outputs)
         for i in range(len(self.Inputs)):
             outputs[i] = not self.Inputs[i]
         self._setOutputs(outputs)
+
+class HalfAdder(LogFunc):
+
+    def __init__(self):
+        super().__init__(2, 2)
+
+    def execute(self):
+        xor = XorGate(2)
+        a = AndGate(2)
+        xor.Inputs = self.Inputs
+        a.Inputs = self.Inputs
+        xor.execute()
+        a.execute()
+        self._setOutputs([xor.Outputs, a.Outputs])
